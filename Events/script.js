52f064b2e825c075359ec046e975468edef33890
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-03-25 18:12:29 +0100

    Add some fun feature to event listener

--- Events/script.js
+++ Events/script.js
@@ -3,4 +3,6 @@ $('.firstButton').click(() => {console.log('Yeah, you clicked me')});
 $('.secondButton').click(() => {
   $('.firstButton').text('You have`clicked on second button')});
 
-$('.thirdButton').click(() => {$('button').css('background-color', 'yellow')});
+// $('.thirdButton').click(() => {$('button').css('background-color', $('input').val())});
+
+$('.thirdButton').one('click', function(){$('button').css('background-color', $('input').val())})
