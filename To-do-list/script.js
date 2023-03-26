edb0ecb64263e179c0aab86f4539c97b2c54f622
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-03-26 23:24:48 +0200

    Fixed click on item to make the text line-through

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,11 +1,14 @@
+$('ul').on( 'click', 'li', function () {
+  $(this).css('text-decoration', 'line-through')
+} );
+
 $('.addTodo').click(()=> $('ul').append(
 `<li class='item'> ${$('.todo').val()}</li>`
 )
 );
 
-$('li.item').on('click', 'li.item', function() {$('li.item').css('text-decoration', 'line-through')});
 
-// $(function(){$('li.item').on('click', 'li.item', function() {$('li.item').css('text-decoration', 'line-through')})});
+
 
 
 
