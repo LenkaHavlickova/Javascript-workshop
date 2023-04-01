d9457d27798479b4551dd624f159e64c3e7678d6
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-04-01 23:22:17 +0200

    Function on done icon

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,10 +1,13 @@
 $('.addButton').click(()=> $('ul').append(
-`<li class='item'> ${$('.todoInput').val()}</li>`
+`<li class='item'> <div class='itemContainer'> ${$('.todoInput').val()} 
+<div class='buttons'>
+<button name='delete' class='deleteButton'> <img class='icon delete' src='assets/icon_trash_bin.png'> </button> <button class='doneButton'> <img class='icon done' src='assets/done_grey_icon.png'> </button> </div> </div></li>`
 )
 );
 
-$('ul').on('click', 'li', function() {$(this).css('text-decoration', 'line-through')});
+$('ul').on('click', 'li', function() {$(this).css('color', 'lightgrey')});
+$('ul').on('click', 'li', function() {$(this).css('font-style', 'italic')});
+$('ul').on('click', '.done', function() {$(this).attr("src", "assets/done_purple_icon.png")});
 
 
-
-
+$('.itemContainer').on('click', '.deleteButton', function() {console.log('test')});
